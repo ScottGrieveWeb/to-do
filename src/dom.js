d3a923c2baea21f676c001ae28cbe583a6d39f64
ScottGrieveWeb <scottgrieveweb@gmail.com>
2024-12-09 22:11:23 +0000

    Initial delete button function added

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,5 @@
+import { removeElementsByClass } from "./remove-element";
+
 export  let domItems = {
     button: document.getElementById("test-btn"),
     toDoDiv: document.getElementById("to-do-list")
@@ -8,6 +10,7 @@ export  const renderProjectItems = function(project){
     for (let i = 0; i < project.items.length; i++){
         let toDoAccordion = document.createElement("details");
         toDoAccordion.classList.add("to-do-item");
+        toDoAccordion.dataset.index = i;
 
         let summary = document.createElement("summary");
         let summaryText = document.createTextNode(`${project.items[i].title}`);
@@ -16,10 +19,27 @@ export  const renderProjectItems = function(project){
         let description = document.createElement("p");
         let descriptionText = document.createTextNode(`${project.items[i].description}`);
         description.appendChild(descriptionText);
+
+        const deleteBttn = document.createElement('button')
+        const deleteTxt = document.createTextNode('delete');
+        deleteBttn.appendChild(deleteTxt);
+        deleteBttn.classList.add("delete");
+        deleteBttn.addEventListener('click' , () => {
+            removeToDo(toDoAccordion.dataset.index, project);
+         }); 
+
         
         toDoAccordion.appendChild(summary);
         toDoAccordion.appendChild(description);
+        toDoAccordion.appendChild(deleteBttn);
 
         domItems.toDoDiv.appendChild(toDoAccordion);
     }
+}
+
+function removeToDo(index, project){
+    project.items.splice(index, 1);
+
+    removeElementsByClass("to-do-item");
+    renderProjectItems(project);
 }
