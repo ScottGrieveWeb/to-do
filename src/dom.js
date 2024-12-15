dacf254c2c4c5a2dadeef7a66040416dd991d5d8
ScottGrieveWeb <scottgrieveweb@gmail.com>
2024-12-15 15:42:51 +0000

    To Do popup now closes on successful submission

--- src/dom.js
+++ src/dom.js
@@ -191,6 +191,7 @@ export const renderProjectList = function(projectList) {
                     removeElementsByClass("project-list");
                     renderProjectList(projectList);
 
+                    domItems.newToDoDialog.close();
                     domItems.dialogForm.reset();
                 }
             }, { once: true});
