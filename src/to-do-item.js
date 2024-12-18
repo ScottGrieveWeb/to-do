f6c2dc032b81e46abf11af9ca198b4939017608f
ScottGrieveWeb <scottgrieveweb@gmail.com>
2024-12-18 22:05:15 +0000

    removed printItem func from to-do-item class

--- src/to-do-item.js
+++ src/to-do-item.js
@@ -1,18 +1,10 @@
 export class ToDoItem {
-    constructor(title, description, dueDate, priority, project) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.project = project;
-        this.completeStatus = false;
-    }
-
-    printItem(){
-        console.log(`Title: ${this.title}`);
-        console.log(`Description: ${this.description}`);
-        console.log(`Due: ${this.dueDate}`);
-        console.log(`Priority: ${this.priority}`);
-        console.log(`Project: ${this.project}`);
-    }
+  constructor(title, description, dueDate, priority, project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+    this.completeStatus = false;
+  }
 }
